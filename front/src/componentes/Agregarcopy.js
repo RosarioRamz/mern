bcaaa56b11815b2d414adfd7fd32ea1e3c77f788
Rosario <dayros.@gmail.com>
2023-02-01 12:31:12 -0600

    Entrega final

--- front/src/componentes/Agregarcopy.js
+++ front/src/componentes/Agregarcopy.js
@@ -24,7 +24,7 @@ function Agregar() {
                 estado,
                 pacientes
             })
-            navigate("/listar")
+            navigate("/inicio")
         }catch(error){
             console.log(error)
         }
